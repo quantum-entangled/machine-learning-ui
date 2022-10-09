033f3e4bba2d0c4454a0ea1a593d8a8f2de0d122
Ivan Shalamov <i.y.shalamov@gmail.com>
2022-10-09 20:46:19 +0200

    Create DataGrid.py file

---
+++ src/UI/DataPreperationWidgets/DataGrid.py
@@ -0,0 +1,1 @@
+from ipydatagrid import DataGrid
--- src/UI/DataPreperationWidgets/UploadFile.py
+++ src/UI/DataPreperationWidgets/UploadFile.py
@@ -6,13 +6,13 @@ class UploadFile(AppLayout):
     def __init__(self, **kwargs):
 
         self.header = Label(value="Please, select your data file:")
-        self.uploader = FileChooser(
+        self.file_chooser = FileChooser(
             path="db/Datasets", sandbox_path="db", filter_pattern="*.txt"
         )
 
         super().__init__(
             header=self.header,
-            center=self.uploader,
+            center=self.file_chooser,
             justify_items="center",
             align_items="center",
             **kwargs
