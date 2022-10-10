2f553904113ed57730c3008599b7636ff5474003
Ivan Shalamov <i.y.shalamov@gmail.com>
2022-10-10 13:05:57 +0200

    Refactor UploadFile widget

--- src/UI/DataPreperationUI.py
+++ src/UI/DataPreperationUI.py
@@ -6,6 +6,7 @@ from .DataPreperationWidgets import *
 class DataPreperationUI(Tab):
     def __init__(self, **kwargs):
         self.uploader = UploadFile()
+
         self.tab_children = [self.uploader]
 
         super().__init__(children=self.tab_children, **kwargs)
--- src/UI/DataPreperationWidgets/UploadFile.py
+++ src/UI/DataPreperationWidgets/UploadFile.py
@@ -1,19 +1,47 @@
+import pandas as pd
 from ipyfilechooser import FileChooser
-from ipywidgets import AppLayout, Label
+from IPython.display import clear_output
+from ipywidgets import Button, Label, Layout, Output, VBox
 
 
-class UploadFile(AppLayout):
+class UploadFile(VBox):
     def __init__(self, **kwargs):
 
         self.header = Label(value="Please, select your data file:")
         self.file_chooser = FileChooser(
             path="db/Datasets", sandbox_path="db", filter_pattern="*.txt"
         )
+        self.upload_button = Button(description="Upload File")
+        self.upload_status = Output()
+        self.file = None
+        self.widget_children = [
+            self.header,
+            self.file_chooser,
+            self.upload_button,
+            self.upload_status,
+        ]
+        self.widget_layout = Layout(
+            height="auto", width="auto", justify_items="center", align_items="center"
+        )
+        self.widget_label = "Upload File"
+
+        self.upload_button.on_click(self.__upload_file)
 
         super().__init__(
-            header=self.header,
-            center=self.file_chooser,
-            justify_items="center",
-            align_items="center",
-            **kwargs
+            children=self.widget_children, layout=self.widget_layout, **kwargs
         )
+
+    def __get_file_path(self):
+        return self.file_chooser.selected
+
+    def __upload_file(self, *args):
+        try:
+            file_path = self.__get_file_path()
+            self.file = pd.read_csv(file_path, sep="  ", engine="python")
+            with self.upload_status:
+                clear_output()
+                print("You're file is successfully uploaded!\u2705")
+        except ValueError:
+            with self.upload_status:
+                clear_output()
+                print("You're file is not selected!\u274C")
