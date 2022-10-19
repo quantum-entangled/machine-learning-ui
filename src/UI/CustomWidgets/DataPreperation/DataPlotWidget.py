2655e47f7ba2b0a30e892713026dbd764a81d146
Ivan Shalamov <i.y.shalamov@gmail.com>
2022-10-19 18:03:22 +0200

    Update DataPlotWidget

--- src/UI/CustomWidgets/DataPreperation/DataPlotWidget.py
+++ src/UI/CustomWidgets/DataPreperation/DataPlotWidget.py
@@ -1,9 +1,10 @@
 from functools import partial
 from typing import Any, Protocol
 
-from bqplot import Axis, Figure, LinearScale, Lines
+from bqplot import Toolbar
+from bqplot import pyplot as plt
 from IPython.display import display
-from ipywidgets import Button, Dropdown, Output, TwoByTwoLayout, VBox, link
+from ipywidgets import Button, Dropdown, Output, TwoByTwoLayout, VBox
 
 
 class File(Protocol):
@@ -13,6 +14,7 @@ class File(Protocol):
 
 
 class DataPlotWidget(VBox):
+    """Widget to display a data plot."""
 
     show_plot_button = Button(description="Show Data Plot")
     plot_output = Output()
@@ -20,40 +22,42 @@ class DataPlotWidget(VBox):
     widget_children = [show_plot_button, plot_output]
 
     def __init__(self, data_file: File, **kwargs) -> None:
-
+        """Initialize the data plot widget window."""
         self.show_plot_button.on_click(partial(show_plot, data_file=data_file))
 
         super().__init__(children=self.widget_children, **kwargs)
 
 
 @DataPlotWidget.plot_output.capture(clear_output=True, wait=True)
-def show_plot(*args, data_file: File):
+def show_plot(*args, data_file: File) -> None:
+    """Show plot of the given file features."""
     try:
         headers = data_file.headers
         dropdown_options = [(header, pos) for pos, header in enumerate(headers)]
-
         x_dropdown = Dropdown(description="x", options=dropdown_options, value=0)
         y_dropdown = Dropdown(description="y", options=dropdown_options, value=0)
 
+        fig = plt.figure()
+        plt.plot(
+            data_file.file[:, x_dropdown.value],
+            data_file.file[:, y_dropdown.value],
+            figure=fig,
+        )
+        plt.xlabel(headers[x_dropdown.value])
+        plt.ylabel(headers[y_dropdown.value])
+
         def on_dropdown_value_change(*args):
-            x_sc = LinearScale()
-            y_sc = LinearScale()
-            ax_x = Axis(label="x", scale=x_sc)
-            ax_y = Axis(label="y", scale=y_sc, orientation="vertical")
-            line = Lines(
-                x=data_file.file[:, x_dropdown.value],
-                y=data_file.file[:, y_dropdown.value],
-                scales={"x": x_sc, "y": y_sc},
-            )
-            fig = Figure(axes=[ax_x, ax_y], marks=[line])
-            plot_window.bottom_right = fig
+            plt.current_figure().marks[0].x = data_file.file[:, x_dropdown.value]
+            plt.current_figure().marks[0].y = data_file.file[:, y_dropdown.value]
+            plt.xlabel(headers[x_dropdown.value])
+            plt.ylabel(headers[y_dropdown.value])
 
         x_dropdown.observe(on_dropdown_value_change, names="value")
         y_dropdown.observe(on_dropdown_value_change, names="value")
 
         plot_window = TwoByTwoLayout(
-            top_left=x_dropdown,
-            bottom_left=y_dropdown,
+            top_left=VBox([x_dropdown, y_dropdown]),
+            top_right=VBox([fig, Toolbar(figure=fig)]),
             align_items="center",
             height="auto",
             width="auto",
