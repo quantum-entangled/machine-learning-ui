40c660f29248535bd702e437b8831c03cb12ff82
Ivan Shalamov <i.y.shalamov@gmail.com>
2022-11-26 23:43:55 +0100

    Add Error and Success Enums

---
+++ src/Enums/ErrorMessages.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+
+class Error(Enum):
+    NO_FILE_PATH = "Please, select the file first!\u274C"
+    NO_FILE_UPLOADED = "Please, upload the file first!\u274C"
+    NO_MODEL_NAME = "Please, enter the model name first!\u274C"
+    SAME_MODEL_NAME = (
+        "Model with this name already exists. Please, enter a different name!\u274C"
+    )
+    NO_MODEL_PATH = "Please, select the model first!\u274C"
+    NO_MODEL_CREATED = "Please, create the model first!\u274C"
+    NO_LAYER_NAME = "Please, enter the layer name first!\u274C"
+    SAME_LAYER_NAME = (
+        "Layer with this name already exists. Please, enter a different name!\u274C"
+    )
+    NO_LAYERS = "There are no layers in the model!\u274C"
+    NO_MODEL_UPLOADED = "Please, upload the model first!\u274C"
+    LAYER_OVERFILLED = "You've selected more columns than the layer can accept!\u274C"
+    MODEL_NOT_TRAINED = "Please, train the model first!\u274C"
+    NO_OUTPUT_LAYERS = "There are no output layers in the model!\u274C"
+    DATA_NOT_SPLITTED = "Please, split the data first!\u274C"
+    NO_MODEL_COLUMNS = "Please, select the model columns first!\u274C"
+    LAYER_UNDERFILLED = "Please, fill all layers with data columns first!\u274C"
+    NO_OPTIMIZER = "Please, select the optimizer first!\u274C"
+    NO_LOSS = "Please, select the loss function(s) first!\u274C"
+
+    def __str__(self):
+        return f"{self.value}"
---
+++ src/Enums/SuccessMessages.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class Success(Enum):
+    FILE_UPLOADED = "File has been successfully uploaded!\u2705"
+    MODEL_CREATED = "Model has been successfully created!\u2705"
+    MODEL_UPLOADED = "Model has been successfully uploaded!\u2705"
+    LAYER_ADDED = "Layer has been successfully added!\u2705"
+    MODEL_SAVED = "Model has been successfully saved!\u2705"
+    COLUMNS_ADDED = "Columns have been successfully added!\u2705"
+    CALLBACK_ADDED = "Callback has been successfully added!\u2705"
+    LOSS_ADDED = "Loss function has been successfully added!\u2705"
+    METRIC_ADDED = "Metric has been successfully added!\u2705"
+    OPTIMIZER_SELECTED = "Optimizer has been successfully selected!\u2705"
+
+    def __str__(self):
+        return f"{self.value}"
