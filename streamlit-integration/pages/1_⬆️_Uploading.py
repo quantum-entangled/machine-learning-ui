98f67e37335dfbda20897cda619c39661e6ab28d
Ivan Shalamov <i.y.shalamov@gmail.com>
2023-05-17 13:15:09 +0200

    Unite uploaders into one module

--- streamlit-integration/pages/1_⬆️_Uploading.py
+++ streamlit-integration/pages/1_⬆️_Uploading.py
@@ -1,14 +1,13 @@
 import streamlit as st
-import widgets.upload.upload_file as up_file
-import widgets.upload.upload_model as up_model
 from data_classes.data import Data
 from data_classes.model import Model
+from widgets.upload import upload as up
 
 if "data" not in st.session_state or "model" not in st.session_state:
     st.session_state.data = Data()
     st.session_state.model = Model()
 
 with st.container():
-    up_file.create_upload_file_ui(st.session_state.data)
+    up.create_upload_file_ui(st.session_state.data)
     st.divider()
-    up_model.create_upload_model_ui(st.session_state.model)
+    up.create_upload_model_ui(st.session_state.model)
--- streamlit-integration/widgets/upload/upload.py
+++ streamlit-integration/widgets/upload/upload.py
@@ -1,9 +1,29 @@
 import tempfile
 
 import streamlit as st
+from data_classes.data import Data
 from data_classes.model import Model
 
+import managers.data_manager as dm
 import managers.model_manager as mm
+
+
+def create_upload_file_ui(data: Data) -> None:
+    st.header("Upload File")
+    uploaded_file = st.file_uploader("Choose a data file:", "csv", key="file_uploader")
+
+    if uploaded_file:
+        try:
+            dm.upload_file(uploaded_file, data)
+            st.success("File is uploaded and ready to be processed!", icon="✅")
+        except dm.UploadError as error:
+            st.exception(error)
+
+    if dm.file_exists(data):
+        st.info(
+            "View the existing file on Data Preparation page or upload a new one.",
+            icon="💡",
+        )
 
 
 def create_upload_model_ui(model: Model) -> None:
