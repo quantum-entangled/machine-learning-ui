2a5777d695fa7025aae80a7e463c6a3af921aad5
daffeu <43210850+daffeu@users.noreply.github.com>
2023-10-22 17:47:15 +0300

    Delete src/mlui directory

--- src/mlui/enums/activations.py
+++
@@ -1,9 +0,0 @@
-from typing import Callable
-
-import tensorflow as tf
-
-classes: dict[str, Callable[..., tf.Tensor]] = {
-    "Linear": tf.keras.activations.linear,
-    "Tanh": tf.keras.activations.tanh,
-    "ReLU": tf.keras.activations.relu,
-}
