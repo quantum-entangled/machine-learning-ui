cb59cf90925e3063715e3a2c6053e98608204d87
daffeu <43210850+daffeu@users.noreply.github.com>
2023-10-22 17:47:01 +0300

    Delete tests directory

--- tests/test_model_manager.py
+++
@@ -1,8 +0,0 @@
-from mlui.data_classes import model as model_cls
-from mlui.managers import model_manager as mm
-
-
-def test_model_exists(model: model_cls.Model) -> None:
-    exists = mm.model_exists(model)
-
-    assert exists is False
